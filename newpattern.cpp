a264651cb8e371c64a52c49127ae087468d8c0a3
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-06 16:40:12 +0530

    Add files via upload

---
+++ newpattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () 
+{ int w = 40; 
+    while(1){ 
+    for (int i=1;i<=w*(w+1);i++) 
+        printf( 
+            "%c", 
+            i % (w+1) == 0 ? '\n' : 
+            i % (w) == 0 || i % (w+2) == 1 ? '*' : ' '); 
+}
+return 0;
+} 

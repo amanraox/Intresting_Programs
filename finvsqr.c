860015f1c1cfbb0a497e03a9b92e5f7d4be8a4df
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-17 02:21:30 +0530

    Update finvsqr.c

--- finvsqr.c
+++ finvsqr.c
@@ -7,3 +7,10 @@ float InvSqrt(float x) {
     x = x * (1.5f - xhalf * x * x); 
     return x; 
 } 
+/*
+
+intresting bit manipulation method 3–4 times faster, with 1/10th the error.
+And also one about 80% the speed with about <0.01% of the error.
+
+A processor that would make this obsolete was released in 1999, when Intel introduced a lot of new commands, meaning that the “hack” was no longer useful because the hardware beat it.
+*/

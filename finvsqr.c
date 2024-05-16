0c3f2d8b0f162b450fbfbbac43ec047fec29183c
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-17 02:26:11 +0530

    Update finvsqr.c

--- finvsqr.c
+++ finvsqr.c
@@ -26,4 +26,5 @@ intresting bit manipulation method 3–4 times faster, with 1/10th the error.
 And also one about 80% the speed with about <0.01% of the error.
 
 A processor that would make this obsolete was released in 1999, when Intel introduced a lot of new commands, meaning that the “hack” was no longer useful because the hardware beat it.
+0x5f3759df - ( i >> 1 )    This single line of code reduced 10x the power of what gpu used to perform and made one of the best game of those time (Quake 3)
 */

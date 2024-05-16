2da2f60e823c028bb5638d8bf149d543c2b6ad78
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-17 02:18:00 +0530

    Create finvsqr.c

---
+++ finvsqr.c
@@ -0,0 +1,9 @@
+// Author: John carmack
+float InvSqrt(float x) { 
+    float xhalf = 0.5f * x; 
+    int i = *(int*)&x; 
+    i = 0x5f3759df - (i >> 1); 
+    x = *(float*)&i; 
+    x = x * (1.5f - xhalf * x * x); 
+    return x; 
+} 
